4fced04f1f666e66a424ab2a393ab57b157d6914
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-02-05 06:51:33 -0800

    Nolu

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate a perimeter as an integer."""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{name} must be greater than 0".format(name))
