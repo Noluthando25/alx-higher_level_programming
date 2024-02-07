31c7caf86a9127dd1f4a91850eeadd820edbccf9
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-02-07 00:00:59 -0800

    Nolu

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
 class Rectangle:
+    """Represents a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new rectangle."""
         self.__width = width
         self.__height = height
 
         @property
         def width(self):
+            """Set the width of rectangle."""
             return self.__width
+
         @width.setter
         def width(self, value):
+
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             if value < 0:
