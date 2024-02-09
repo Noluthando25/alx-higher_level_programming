baa5c79d065317e117e3442e558f2e98bd4ac9f6
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-02-09 09:17:47 -0800

    Nolu

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a integer."""
 
+
 def add_integer(a, b=98):
     """Represent a integer."""
 
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a matrix."""
+
+
+def matrix_divided(matrix, div):
+    # Check if matrix is a list of lists of integers or floats
+    if not all(isinstance(row, list) for row in matrix) or not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Check if each row of the matrix has the same size
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    # Check if div is a number (integer or float)
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number (integer or float)")
+
+    # Perform the division operation on each element of the matrix
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
+    return new_matrix
