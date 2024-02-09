dc7f342c510bd05b65062cb1b92596461d5ddc2b
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-02-09 09:20:30 -0800

    Nolu

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,8 @@
 
 
 def matrix_divided(matrix, div):
-    # Check if matrix is a list of lists of integers or floats
+    """Represent a new matrix."""
+    
     if not all(isinstance(row, list) for row in matrix) or not all(isinstance(num, (int, float)) for row in matrix for num in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
