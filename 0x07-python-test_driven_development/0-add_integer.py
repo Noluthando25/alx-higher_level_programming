d8482ddca5380450c807a5f7c677997717e29c8e
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-02-09 09:03:38 -0800

    Nolu

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a integer."""
+
+def add_integer(a, b=98):
+    """Represent a integer."""
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
