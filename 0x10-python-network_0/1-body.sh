e103537590953c6bde43277b1969b113eee6c2f6
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-04-25 13:24:40 -0700

    Nolu

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 # Get the URL from user input
 read -p "Enter the URL: " url
-
-# Send a GET request to the URL and display the response body for a 200 status code
 response=$(curl -s -w "%{http_code}" $url)
-
 status_code=${response: -3}
-
 if [ $status_code -eq 200 ]; then
-    content=$(curl -s $url)
-    echo "Response body:"
-    echo "$content"
+	content=$(curl -s $url)
+	echo "Response body:"
+	echo "$content"
 else
-    echo "Error: Received status code $status_code, unable to display response body."
+	echo "Error: Received status code $status_code, unable to display response body."
 fi
