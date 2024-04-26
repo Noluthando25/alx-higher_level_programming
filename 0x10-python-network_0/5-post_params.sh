e5faea9447ea5267553c34c2bcacc9bc1523b84c
Noluthando25 <nonhlanhlafumba@gmsil.com>
2024-04-26 09:55:35 -0700

    Nolu

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Get the URL from user input
 read -p "Enter the URL: " url
-email="test@gmail.com"
-subject="I will always be here for PLD"
 response=$(curl -s -X POST -d "email=$email&subject=$subject" $url)
-echo "Response body:"
-echo "$response"
+
