4d0017b3474346d403661d60b243a3554f20a632
Noluthando25 <nonhlanhlafumba@gmsil.com>
2024-04-26 09:41:49 -0700

    Nolu

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Get the URL from the user input
 read -p "Enter the URL: " url
 response=$(curl -s -o /dev/null -w "%{size_download}" $url)
 echo "Response body size: $response bytes"
