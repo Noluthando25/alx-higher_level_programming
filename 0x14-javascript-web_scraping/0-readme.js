79e9a17c9c084394f535f3e616e2fcfbd486d653
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-05-22 13:57:54 -0700

    Nolu

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,15 +3,15 @@
 const fs = require('fs');
 // Import the built in node.
 
-fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
 	// Use fs.readFile
-	// 'utf*' specifies the encoding of the file being read
+	// 'utf8' specifies the encoding of the file being read
 
     if (error) {
 	    // An error message will be shown.
-        console.error('Error occurred while reading the file:' error);
+	    console.error('Error occurred while reading the file:' error);
     } else {
 	    // If successful it will contain the content of the file
-        console.log(content);
+	    console.log(content);
     }
 });
