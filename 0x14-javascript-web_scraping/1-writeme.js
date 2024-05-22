1e346963119f6e925a563be62259adf1c3262a90
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-05-22 14:26:24 -0700

    Nolu

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,12 @@
 const fs = require('fs');
 // Import the built in node.js
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', err => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
 	// Write data to a file specified
 	// Process.argv[3].
 
-	if (err) {
+	if (error) {
 		// The error perimeter will contain an error message. 
-		console.error(err); 
+		console.error(error); 
 	}
 });
