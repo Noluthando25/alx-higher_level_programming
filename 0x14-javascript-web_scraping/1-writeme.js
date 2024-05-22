1773d00c330599fdb72e990b3cd5f0d402ffa259
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-05-22 13:39:49 -0700

    Nolu

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,17 +2,16 @@
 
 const fs = require('fs');
 
-const writeStringToFile = (filePath, content) =>
-{
-    fs.writeFile(filePath, content, 'utf8', (err) =>
-	    {
-		    if (err)
-		    {
-			    console.error(err);
-		    }
-		    else
-		    {
-			    console.log('String successfully written to file.');
-		    }
-	    });
-});
+const writeStringToFile = (filePath, content) => {
+    fs.writeFile(filePath, content, 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('String successfully written to file.');
+        }
+    });
+};
+
+const filePath = 'example.txt';
+const content = 'Hello, world!';
+writeStringToFile(filePath, content);
