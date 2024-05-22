6de7f0d18ee42834d9023c0dce0fd2bf95b7f7e8
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-05-22 14:17:26 -0700

    Nolu

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+!/usr/bin/node
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', err => {
-        if (err) {
-            console.error(err);
-        }  
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', err => { 
+	if (err) { 
+		console.error(err); 
+	}
 });
